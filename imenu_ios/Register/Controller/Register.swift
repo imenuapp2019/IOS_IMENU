9d324ab88959696e2468f25164937b28a8bdea94
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-01-09 19:56:43 +0100

    ViewRatio

---
+++ imenu_ios/Register/Controller/Register.swift
@@ -0,0 +1,25 @@
+//
+//  Register.swift
+//  imenu_ios
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 09/01/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import UIKit
+
+class Register: UIViewController {
+
+    @IBOutlet weak var innerView: UIView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    //innerView.layer.cornerRadius = 10
+
+        
+    }
+    
+
+    
+
+}
