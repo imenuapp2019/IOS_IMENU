1903144c8129f546efc6925c2697c610a9d5275c
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-01-10 19:24:45 +0100

    [ET] Avances Register 2
    Avances Register 2

--- imenu_ios/Register/Controller/RegisterViewController.swift
+++ imenu_ios/Register/Controller/RegisterViewController.swift
@@ -11,8 +11,7 @@ import SkyFloatingLabelTextField
 
 class RegisterViewController: UIViewController {
     
-    let greenColor:UIColor = UIColor(red: 38/255, green: 128/255, blue: 38/255, alpha: 1)
-     let whiteColor:UIColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+
 
     @IBOutlet weak var registerAvatarImageView: UIImageView!
     
@@ -34,7 +33,9 @@ class RegisterViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        
+        let tap = hideKeyboard()
+        view.addGestureRecognizer(tap)
+        moveScreenWhenUseKeyboard()
         
         
         
@@ -55,20 +56,87 @@ class RegisterViewController: UIViewController {
         
         nameTextField.placeholder = Literals.placedeHolderRegisterName
         nameTextField.title = Literals.placedeHolderRegisterName
+        nameTextField.tintColor = Color.greenBtnColor
+        nameTextField.lineColor = Color.greenBtnColor
+        nameTextField.selectedLineColor = Color.greenBtnColor
+        nameTextField.lineHeight = 2.0
+        nameTextField.selectedLineHeight = 3.0
+        
+        
         lastNameTextField.placeholder = Literals.placedeHolderRegisterLastName
         lastNameTextField.title = Literals.placedeHolderRegisterLastName
+        lastNameTextField.tintColor = Color.greenBtnColor
+        lastNameTextField.lineColor = Color.greenBtnColor
+        lastNameTextField.selectedLineColor = Color.greenBtnColor
+        lastNameTextField.lineHeight = 2.0
+        lastNameTextField.selectedLineHeight = 3.0
+        
+        
         emailTextField.placeholder = Literals.placedeHolderRegisterEmail
         emailTextField.title = Literals.placedeHolderRegisterEmail
+        emailTextField.tintColor = Color.greenBtnColor
+        emailTextField.lineColor = Color.greenBtnColor
+        emailTextField.selectedLineColor = Color.greenBtnColor
+        emailTextField.lineHeight = 2.0
+        emailTextField.selectedLineHeight = 3.0
+        
+        
         passwordTextField.placeholder = Literals.placedeHolderRegisterPassword
         passwordTextField.title = Literals.placedeHolderRegisterPassword
+        passwordTextField.tintColor = Color.greenBtnColor
+        passwordTextField.lineColor = Color.greenBtnColor
+        passwordTextField.selectedLineColor = Color.greenBtnColor
+        passwordTextField.lineHeight = 2.0
+        passwordTextField.selectedLineHeight = 3.0
+        
+        
         vrfPasswordTextField.placeholder = Literals.placedeHolderRegisterVrfPassword
         vrfPasswordTextField.title = Literals.placedeHolderRegisterVrfPassword
+        vrfPasswordTextField.tintColor = Color.greenBtnColor
+        vrfPasswordTextField.lineColor = Color.greenBtnColor
+        vrfPasswordTextField.selectedLineColor = Color.greenBtnColor
+        vrfPasswordTextField.lineHeight = 2.0
+        vrfPasswordTextField.selectedLineHeight = 3.0
+        
+        
         
         confirmBtn.setTitle(Literals.ConfirmRegisterBtn, for: .normal)
-        confirmBtn.tintColor = whiteColor
-        confirmBtn.backgroundColor = greenColor
+        confirmBtn.tintColor = Color.whiteColor
+        confirmBtn.backgroundColor = Color.greenBtnColor
+        confirmBtn.titleLabel?.font = UIFont.boldSystemFont(ofSize: 20)
         
+       
         
     }
 
+}
+
+extension RegisterViewController {
+   
+  func moveScreenWhenUseKeyboard(){
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+  }
+   
+  func hideKeyboard()->UITapGestureRecognizer{
+    let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+    return tap
+  }
+   
+  @objc func dismissKeyboard() {
+    view.endEditing(true)
+  }
+   
+  @objc func keyboardWillShow(notification: NSNotification) {
+    if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameBeginUserInfoKey] as? NSValue)?.cgRectValue {
+      if self.view.frame.origin.y == 0 {
+        self.view.frame.origin.y -= keyboardSize.height
+      }
+    }
+  }
+  @objc func keyboardWillHide(notification: NSNotification) {
+    if self.view.frame.origin.y != 0 {
+      self.view.frame.origin.y = 0
+    }
+  }
 }
