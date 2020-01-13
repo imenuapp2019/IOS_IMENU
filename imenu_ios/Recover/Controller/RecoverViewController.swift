aedd50ff0529c78c2cdc11434757d1f066867516
Miguel <migueljaimes1405@gmail.com>
2020-01-13 23:06:02 +0100

    [MJ][Recover - Vista]
    Agregando vista y controlador del recover + los elementos

--- imenu_ios/Recover/Controller/RecoverViewController.swift
+++ imenu_ios/Recover/Controller/RecoverViewController.swift
@@ -7,9 +7,18 @@
 //
 
 import UIKit
+import SkyFloatingLabelTextField
 
 class RecoverViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var backgroundImage: UIImageView!
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var cardView: UIView!
+    @IBOutlet weak var labelTextRecoverEmail: UILabel!
+    @IBOutlet weak var inputlabelEmail: SkyFloatingLabelTextFieldWithIcon!
+    @IBOutlet weak var btnRecover: RoundButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
