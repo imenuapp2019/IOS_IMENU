51c2728a7e46e76ecf7118764bfc78ad7ff9f8c2
Miguel <migueljaimes1405@gmail.com>
2020-01-16 19:28:50 +0100

    [MJ][APIManager]

--- imenu_ios/APIManager/APIManager.swift
+++ imenu_ios/APIManager/APIManager.swift
@@ -7,11 +7,24 @@
 //
 
 import Foundation
+import Alamofire
 
 class APIManager {
     
     func postLogin(Email email:String, Password password:String){
         
+        // parameters that are needed to be posted in the backend
+        let params = [
+            "email": email,
+            "password": password
+        ] as [String : Any]
         
+        Alamofire.request("", method: .post, parameters: params, headers: headers).responseString  { response in
+            print(response.request) // original url request
+            print(response.response) // http url reponse
+            if let json = response.result.value {
+               print("JSON: \(json)") // serialized json response after post
+            }       
+        }
     }
 }
