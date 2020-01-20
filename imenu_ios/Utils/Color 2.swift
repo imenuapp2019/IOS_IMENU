07df7de69d50ba1912c556dc5df17e891c0b4f5c
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-01-20 19:25:28 +0100

    Release 1
    release 1

---
+++ imenu_ios/Utils/Color 2.swift
@@ -0,0 +1,17 @@
+//
+//  Color.swift
+//  imenu_ios
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 10/01/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import  UIKit
+
+
+open class Color {
+    static var greenBtnColor:UIColor = UIColor(red: 38/255, green: 128/255, blue: 38/255, alpha: 1)
+    static  var whiteColor:UIColor = UIColor(red: 255/255, green: 255/255, blue: 255/255, alpha: 1)
+    static  var redColor:UIColor = UIColor(red: 255/255, green: 0/255, blue: 0/255, alpha: 1)
+    static var grayBtnColor: UIColor = UIColor(red: 136/255, green: 136/255, blue: 136/255, alpha: 1)
+}
