897d437534051cbbb3dffee47b3d98539d2752f4
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-01-21 18:14:38 +0100

    Starting Sprint 2

---
+++ imenu_ios/Model/Dish.swift
@@ -0,0 +1,9 @@
+//
+//  Dish.swift
+//  imenu_ios
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 21/01/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import Foundation
