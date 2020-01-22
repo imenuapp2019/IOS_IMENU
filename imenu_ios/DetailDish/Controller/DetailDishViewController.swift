779e3dcd4498c2fcd6d04bf56d19553de55bfbaf
Miguel <migueljaimes1405@gmail.com>
2020-01-22 17:02:10 +0100

    [MJ][ Montando Modulos]

---
+++ imenu_ios/DetailDish/Controller/DetailDishViewController.swift
@@ -0,0 +1,30 @@
+//
+//  DetailDishViewController.swift
+//  imenu_ios
+//
+//  Created by Miguel Jaimes on 22/01/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import UIKit
+
+class DetailDishViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
