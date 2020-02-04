18e9f6a2e3a93598af0732c1e3f70efc0acf8d62
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-02-04 18:33:24 +0100

    [ET] [Maquetado inicial profile]

---
+++ imenu_ios/Profile/Controller/ProfileViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileViewController.swift
+//  imenu_ios
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 04/02/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
