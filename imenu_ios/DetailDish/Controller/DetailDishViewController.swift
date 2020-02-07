8fc0fe90c32867d4484c81f2adb57cb46bd2b613
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-02-07 18:20:52 +0100

    mielda

--- imenu_ios/DetailDish/Controller/DetailDishViewController.swift
+++ imenu_ios/DetailDish/Controller/DetailDishViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class DetailDishViewController: UIViewController {
-
-    
+class DetailDishViewController: UIViewController, UICollectionViewDelegateFlowLayout,UICollectionViewDataSource, UICollectionViewDelegate {
+   
+    @IBOutlet weak var collectionView: UICollectionView!
+    var images:[UIImage] = [#imageLiteral(resourceName: "Capa 6"),#imageLiteral(resourceName: "Capa 5"),#imageLiteral(resourceName: "Capa 4"),#imageLiteral(resourceName: "Capa 3"),#imageLiteral(resourceName: "Capa 2"),#imageLiteral(resourceName: "Capa 7")]
     @IBOutlet weak var likebtnView: UIView!
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var dishPriceLabel: UILabel!
@@ -20,7 +21,8 @@ class DetailDishViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
             self.title = "Detalle"
-        
+        collectionView.dataSource = self
+        collectionView.delegate = self
         cardView.layer.cornerRadius = 15
         cardView.layer.masksToBounds = true
         dishPriceLabel.layer.cornerRadius = 15
@@ -29,5 +31,16 @@ class DetailDishViewController: UIViewController {
         innerCard.layer.masksToBounds = true
        likebtnView.layer.cornerRadius = likebtnView.frame.height/2
     }
-    
+    public func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+                 4
+             }
+             
+       public func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+                 let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellDish", for: indexPath) as! DishesSliderCollectionViewCell
+        
+        cell.dishesImageView.image = images [indexPath.item]
+              return cell
+             }
+          
+
 }
