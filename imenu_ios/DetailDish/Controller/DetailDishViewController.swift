6cf8e6febadabe8876eb825c70b3d384c9bb23a2
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-02-12 19:44:37 +0100

    DetailViewDone

--- imenu_ios/DetailDish/Controller/DetailDishViewController.swift
+++ imenu_ios/DetailDish/Controller/DetailDishViewController.swift
@@ -9,14 +9,38 @@
 import UIKit
 
 class DetailDishViewController: UIViewController, UICollectionViewDelegateFlowLayout,UICollectionViewDataSource, UICollectionViewDelegate {
-   
+    
+ 
+
+    @IBAction func detailBtnAction(_ sender: Any) {
+        showDeatailView ()
+
+    }
+    
+    @IBAction func alertBtnAction(_ sender: Any) {
+      ShowAlergView()
+    }
+
+    @IBOutlet weak var alergView: UIView!
+    @IBOutlet weak var detailTextView: UITextView!
+    
+    @IBOutlet weak var alerBtnView: UIView!
+    @IBOutlet weak var detailBtnView: UIView!
+    
+    @IBOutlet weak var textView: UITextView!
     @IBOutlet weak var collectionView: UICollectionView!
     var images:[UIImage] = [#imageLiteral(resourceName: "HD_tarta"),#imageLiteral(resourceName: "HDpollo"),#imageLiteral(resourceName: "HD_Pizza"),#imageLiteral(resourceName: "HDpollo"),#imageLiteral(resourceName: "HD_Pizza"),#imageLiteral(resourceName: "HD_tarta")]
+    var liked:Bool = false
     @IBOutlet weak var likebtnView: UIView!
     @IBOutlet weak var cardView: UIView!
     @IBOutlet weak var dishPriceLabel: UILabel!
     @IBOutlet weak var innerCard: UIView!
     @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var heartImageView: UIImageView!
+    @IBAction func heartBtnAction(_ sender: Any) {
+        changeDishLike ()
+    }
+    
     var currentIndex = 0
     var timer:Timer?
     
@@ -25,16 +49,7 @@ class DetailDishViewController: UIViewController, UICollectionViewDelegateFlowLa
             self.title = "Detalle"
         collectionView.dataSource = self
         collectionView.delegate = self
-        cardView.layer.cornerRadius = 15
-        cardView.layer.masksToBounds = true
-        dishPriceLabel.layer.cornerRadius = 15
-        dishPriceLabel.layer.masksToBounds = true
-        innerCard.layer.cornerRadius = 15
-        innerCard.layer.masksToBounds = true
-       likebtnView.layer.cornerRadius = likebtnView.frame.height/2
-        pageControl.numberOfPages = images.count
-        startTimer()
-       
+        initialViewConfig ()
         
     }
     
@@ -62,4 +77,50 @@ class DetailDishViewController: UIViewController, UICollectionViewDelegateFlowLa
         currentIndex = Int(scrollView.contentOffset.x / collectionView.frame.size.width)
         pageControl.currentPage = currentIndex
     }
+    
+    func changeDishLike () {
+    
+        if !liked {
+            heartImageView.image = UIImage(named: "corazon_rojo")
+            liked = true
+        }
+        else {
+            heartImageView.image = UIImage(named: "corazon_gris")
+            liked = false
+        }
+    }
+    
+    
+    func showDeatailView () {
+       
+        detailTextView.isHidden = false
+        alergView.isHidden = true
+        detailBtnView.backgroundColor = UIColor.lightGray
+        alerBtnView.backgroundColor = UIColor.white
+    }
+    
+    func ShowAlergView () {
+        alergView.isHidden = false
+        detailTextView.isHidden = true
+        alerBtnView.backgroundColor = UIColor.lightGray
+         detailBtnView.backgroundColor = UIColor.white
+        
+    }
+    func initialViewConfig () {
+        
+        cardView.layer.cornerRadius = 15
+        cardView.layer.masksToBounds = true
+        dishPriceLabel.layer.cornerRadius = 15
+        dishPriceLabel.layer.masksToBounds = true
+        innerCard.layer.cornerRadius = 15
+        innerCard.layer.masksToBounds = true
+        likebtnView.layer.cornerRadius = likebtnView.frame.height/2
+        pageControl.numberOfPages = images.count
+        startTimer()
+        textView.isEditable = false
+        detailBtnView.backgroundColor = UIColor.lightGray
+         alergView.isHidden = true
+        
+    }
+    
 }
