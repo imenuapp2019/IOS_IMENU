50d41c18006d89221ec4bd2a87e4f525421ec561
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-02-24 18:57:50 +0100

    ARCollectionView

---
+++ imenu_ios/Menu/Controller/ARCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ARCollectionViewCell.swift
+//  imenu_ios
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 20/02/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import UIKit
+
+class ARCollectionViewCell: UICollectionViewCell {
+    @IBOutlet weak var arDishesImageView: UIImageView!
+    
+}
--- imenu_ios/Menu/Controller/MenuCardViewController.swift
+++ imenu_ios/Menu/Controller/MenuCardViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 
 class MenuCardViewController: UIViewController, UICollectionViewDataSource,UICollectionViewDelegate {
     
-    
+    let ARImagesArray = [#imageLiteral(resourceName: "HD_Pizza"),#imageLiteral(resourceName: "HDpollo"),#imageLiteral(resourceName: "HD_tarta"),#imageLiteral(resourceName: "HDpollo"),#imageLiteral(resourceName: "HD_Pizza")]
     @IBAction func dishClicked(_ sender: Any) {
         
     }
@@ -19,30 +19,54 @@ class MenuCardViewController: UIViewController, UICollectionViewDataSource,UICol
     @IBOutlet weak var handleArea: UIView!
     @IBOutlet weak var menuSectionsCollectionView: UICollectionView!
     
+    @IBOutlet weak var ARCollectionView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         menuSectionsCollectionView.delegate = self
         menuSectionsCollectionView.dataSource = self
+        ARCollectionView.delegate = self
+        ARCollectionView.dataSource = self
         
         let nibName = UINib(nibName: "CollectionViewCell", bundle:nil)
         menuSectionsCollectionView.register(nibName, forCellWithReuseIdentifier: "section")
         
+        let ARNib = UINib(nibName: "ARSliderCell", bundle:nil)
+        
+        ARCollectionView.register(ARNib, forCellWithReuseIdentifier: "ARCell")
+        
     }
     
+    
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 6
+        
+        if collectionView == menuSectionsCollectionView {
+           return 6
+        }
+        else {
+            
+            return ARImagesArray.count        }
+        
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        collectionView.backgroundColor = .clear
+        if collectionView == menuSectionsCollectionView {
+                collectionView.backgroundColor = .clear
+                   let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "section", for: indexPath) as! MenuCollectionViewCell
+                   cell.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(DoWhenACellIsClicked(_:))))
+                   return  cell as UICollectionViewCell
+            
+        }
+        else {
+             let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "ARCell", for: indexPath) as! ARCollectionViewCell
+            cell.arDishesImageView.image = ARImagesArray [indexPath.row]
+            
+            return cell as UICollectionViewCell
+        }
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "section", for: indexPath) as! MenuCollectionViewCell
-        
-        cell.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(DoWhenACellIsClicked(_:))))
-
-        return  cell as UICollectionViewCell
+       
     }
     
     @objc func DoWhenACellIsClicked(_ sender: UITapGestureRecognizer) {
