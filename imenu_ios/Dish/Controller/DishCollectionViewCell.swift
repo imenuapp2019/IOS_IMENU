9f3a1fe6fe67e11349e09d0b0879504f490da7a4
Miguel Jaimes <migueljaimes1405@gmail.com>
2020-02-29 15:57:59 +0100

    MJ  - Cambios Segujnda parte

--- imenu_ios/Dish/Controller/DishCollectionViewCell.swift
+++ imenu_ios/Dish/Controller/DishCollectionViewCell.swift
@@ -9,42 +9,44 @@
 import UIKit
 
 class DishCollectionViewCell: UICollectionViewCell {
-
-
-  @IBOutlet weak var stackview: UIStackView!
+    
+    
+    @IBOutlet weak var stackview: UIStackView!
     @IBOutlet weak var view1: UIView!
     
     @IBOutlet weak var DishImageView: UIImageView!
     
     @IBOutlet weak var gradientImageView: UIImageView!
-  
+    
     @IBOutlet weak var dishNameLabel: UILabel!
     
     override func layoutSubviews() {
-    super.layoutSubviews()
-    self.layer.cornerRadius = 20
-    self.clipsToBounds = true
-    applyGradient()
-    
+        super.layoutSubviews()
+        self.layer.cornerRadius = 20
+        self.clipsToBounds = true
+        applyGradient()
+        
     }
-    
+
     
     private func applyGradient () {
         
         let view = UIView(frame:DishImageView.frame)
-
+        
         let gradient = CAGradientLayer()
-
+        
         gradient.frame = view.frame
-
+        
         gradient.colors = [UIColor.clear.cgColor, UIColor.black.cgColor]
-
+        
         gradient.locations = [0.0, 1.0]
-
+        
         view.layer.insertSublayer(gradient, at: 0)
-
+        
         gradientImageView.addSubview(view)
-
+        
         gradientImageView.bringSubviewToFront(view)
     }
+    
+   
 }
