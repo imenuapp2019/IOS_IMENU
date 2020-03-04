79611ed9ad004468f5348606f0156b16bccf938a
Eduardo Terrero Cabrera <eduardo.terrero.c@hotmail.com>
2020-03-04 15:45:43 +0100

    3 views

---
+++ imenu_ios/Map/Controller/MapCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  MapCollectionViewCell.swift
+//  imenu_ios
+//
+//  Created by Eduardo Antonio Terrero Cabrera on 03/03/2020.
+//  Copyright © 2020 Miguel Jaimes. All rights reserved.
+//
+
+import UIKit
+
+class MapCollectionViewCell: UICollectionViewCell {
+    
+}
